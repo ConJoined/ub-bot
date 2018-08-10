72f43a7e3a6b41f5aa229ff06ce79fa7a493d85a
IOSuppression <39203143+ConJoined@users.noreply.github.com>
2018-08-10 13:00:02 -0500

    Update ping.js

--- files/ping.js
+++ files/ping.js
@@ -1,12 +1,12 @@
 exports.run = (client, message, args) => {
     const result = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
+  .setTitle("Pong!")
   .setAuthor("Unity Builders Bot", client.user.displayAvatarURL)
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor("#cc66ff")
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setDescription("The ping is " + client.ping + "ms.")
   .setFooter("Requested by: " + message.author.tag, message.author.displayAvatarURL)
   /* .setThumbnail(client.user.displayAvatarURL) */
   /*
