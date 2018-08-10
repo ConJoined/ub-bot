7036fa90cc4406c2ac5669e42f15314032eba2c5
IOSuppression <39203143+ConJoined@users.noreply.github.com>
2018-08-10 12:56:33 -0500

    Update ping.js

--- files/ping.js
+++ files/ping.js
@@ -1,3 +1,18 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Pong! Here's my total heartbeat: " + client.ping + "ms.").catch(console.error);
+    const result = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Unity Builders Bot", client.user.displayAvatarURL)
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor("#cc66ff")
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("Requested by: " + message.author.tag, message.author.displayAvatarURL)
+  /* .setThumbnail(client.user.displayAvatarURL) */
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp();
+​
+  message.channel.send({embed: result});
 }
